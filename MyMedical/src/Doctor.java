72f32d53e389912c1fdc6f9786238dcfe238d43e
Larry Cárdenas Zavaleta <larrycardenaszavaleta@gmail.com>
2021-04-08 23:23:02 +0200

    primera clase

---
+++ MyMedical/src/Doctor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author windows
+ */
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+    
+    public void showName(){
+        System.out.println(name);
+    }
+}
